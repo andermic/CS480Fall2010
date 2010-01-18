40b59410063db833d9facb2167eef40f7aefbc69
Mike Anderson <mike@mike-laptop.(none)>
2010-01-17 19:01:17 -0800

    Completed most of the parser methods, haven't yet tested anything.

---
+++ prog2/Lexer.java
@@ -0,0 +1,219 @@
+//
+//		Compiler for CS 480
+//		Class Lexer
+//
+//		Written by Tim Budd, Winter term 2006
+//
+//		Modified by: Mike Anderson, Sam Heinith, and Rob Mcguire-Dale 
+//
+
+import java.io.*;
+
+//
+//--------Lexer----------------
+//
+
+public class Lexer {
+	private PushbackReader input;
+	private String token;
+	private int tokenType;
+	private String[] keywords = {"not","new","and","or","while","if","return","begin","end",
+	 "function","class","const","type","var","else"};
+	final int EOF_RETURN = 65535; // To fix a Java glitch, read() returns 65535 on eof if
+								  //  ints are longer than 16 bits.
+	                          
+
+	public Lexer(Reader in) {
+		input = new PushbackReader(in);
+	}
+
+	private void skipComments() throws ParseException, IOException {
+		while(true) {
+			int firstChar = 0, middleChar = 0;
+			skipWhiteSpace();
+			
+			try { 
+				firstChar = input.read();
+			} catch (IOException e) { throw new ParseException(0); }
+
+			if( (char)firstChar != '{' ){
+				try { 
+					input.unread(firstChar);
+				} catch (IOException e) { throw new ParseException(0); }
+				break;
+			}
+			
+			do {
+				try { 
+					middleChar = input.read();
+				} catch (IOException e) { throw new ParseException(0); }
+				if( (char)middleChar == '{' || middleChar == -1 || middleChar == EOF_RETURN ) {
+					throw new ParseException(1);
+				}
+			} while( (char)middleChar != '}' );
+		}
+	}
+	
+	private void skipWhiteSpace() throws ParseException, IOException {
+		int currentChar = -1;
+		do { 
+			try { 
+				currentChar = input.read();
+			} catch (IOException e) { throw new ParseException(0); }
+		} while(Character.isWhitespace((char) currentChar));
+		try { 
+			input.unread(currentChar);
+		} catch (IOException e2) { throw new ParseException(0); }
+		return;		
+	}
+	
+	public void nextLex() throws ParseException, IOException {
+		token = "";
+		char currentChar;
+		int currentCharAsInt;
+
+		skipComments();
+		skipWhiteSpace();
+
+		try {
+			 currentCharAsInt = input.read();
+		} catch (IOException e1) { throw new ParseException(0); }
+		
+		currentChar = (char) currentCharAsInt;
+		if( currentCharAsInt == -1 || currentCharAsInt == EOF_RETURN ){
+			tokenType = endOfInput;
+		}		
+		
+		else if(Character.isDigit(currentChar)) {
+			tokenType = intToken; //by default
+			token = token + currentChar;
+			while(true){
+				try {
+					currentChar = (char) input.read();
+				} catch (IOException e) { throw new ParseException(0); }
+				if(Character.isDigit(currentChar)){
+					token = token + currentChar;
+				}
+				else if (currentChar == '.'){
+					if(token.indexOf('.') == -1){
+						tokenType = realToken;
+						token = token + currentChar;
+					} else {
+						try {
+							input.unread(currentChar);
+						} catch (IOException e) { throw new ParseException(0); }
+						break;
+					}
+				}
+				else {
+					try {
+						input.unread(currentChar);
+					} catch (IOException e) { throw new ParseException(0); }
+					break;
+				}				
+			}			
+		}		
+
+		else if(Character.isLetter(currentChar)) { 		
+			token = token + currentChar;
+			while(true){
+				try {
+					currentChar = (char) input.read();
+				} catch (IOException e) { throw new ParseException(0); }
+				if(Character.isLetterOrDigit(currentChar)){
+					token = token + currentChar;
+				}
+				else {
+					input.unread(currentChar);
+					tokenType = identifierToken;
+					for(String current: keywords){
+						if(match(current)){
+							tokenType = keywordToken;
+							break;
+						}
+					}					
+					break;	
+				}
+			}
+		}		
+		
+		else {
+			switch(currentChar) {
+			case '"':
+				tokenType = stringToken;
+					while(true) {
+						try {
+							currentCharAsInt = input.read();
+							currentChar = (char) currentCharAsInt;
+						} catch (IOException e) { throw new ParseException(0); }
+						if( currentCharAsInt == -1 || currentCharAsInt == EOF_RETURN ) {
+							throw new ParseException(2);
+						}
+						else if( currentChar == '"') {
+							break;
+						}
+						token = token + currentChar;
+					} 
+				break;
+				
+			case '<':
+				tokenType = otherToken;
+				token = token + currentChar;
+				char secondChar = (char) input.read();
+				if(secondChar == '<'){
+					token = token + secondChar;
+					break;
+				}
+				else {
+					input.unread(secondChar);
+				}					
+
+			case '>':				
+			case '=':			
+			case '!':
+				tokenType = otherToken;
+				if(currentChar != '<') {
+					token = token + currentChar;
+				}
+				secondChar = (char) input.read();
+				if(secondChar == '=') {
+					token = token + secondChar;
+					break;
+				}
+				else {
+					input.unread(secondChar);
+					}
+				break;
+				
+			default:
+				token = Character.toString(currentChar);
+				tokenType = otherToken;		
+			}
+		}	
+		return;		
+	}
+
+	static final int identifierToken = 1;
+	static final int keywordToken = 2;
+	static final int intToken = 3;
+	static final int realToken = 4;
+	static final int stringToken = 5;
+	static final int otherToken = 6;
+	static final int endOfInput = 7;
+
+	public String tokenText() {
+		return token;
+	}
+
+	public int tokenCategory() {
+		return tokenType;
+	}
+
+	public boolean isIdentifier() {
+		return tokenType == identifierToken;
+	}
+
+	public boolean match (String test) {
+		return test.equals(token);
+		}
+}
--- prog2/Parser.java
+++ prog2/Parser.java
@@ -1,7 +1,7 @@
 //
 //	parser skeleton, CS 480, Winter 2006
 //	written by Tim Budd
-//		modified by:
+//		modified by: Michael Anderson, Sam Heinith, Rob McGuire-Dale
 //
 
 public class Parser {
@@ -29,6 +29,10 @@ public class Parser {
 		throw new ParseException(number);
 	}
 
+	private void parseError(int number, String name) throws ParseException {
+		throw new ParseException(number, name);
+	}
+
 	private void program () throws ParseException {
 		start("program");
 
@@ -42,6 +46,447 @@ public class Parser {
 		stop("program");
 	}
 
-	// your stuff goes here
+	private void declaration () throws ParseException {
+		start("declaration");
+		
+		if (lex.match("class")) {
+			lex.nextLex();
+			classDeclaration();
+		}
+		else {
+			nonClassDeclaration();
+		}
+		
+		stop("declaration");
+	}
 
+	private void nonClassDeclaration() throws ParseException {
+		start("nonClassDeclaration");
+		
+		if (lex.match("function")) {
+			lex.nextLex();
+			functionDeclaration();
+		}
+		else {
+			nonFunctionDeclaration();
+		}
+		
+		stop("nonClassDeclaration");
+	}
+	
+	private void nonFunctionDeclaration() throws ParseException {
+		start("nonFunctionDeclaration");
+		
+		if (lex.match("var")) {
+			lex.nextLex();
+			variableDeclaration();
+		}
+		else if (lex.match("const")){
+			lex.nextLex();
+			constDeclaration();
+		}
+		else if (lex.match("type")){
+			lex.nextLex();
+			typeDeclaration();
+		}
+		else {
+			parseError(26);
+		}
+		
+		stop("nonFunctionDeclaration");
+	}
+	
+	private void constantDeclaration() throws ParseException {
+		start("constantDeclaration");
+		
+		if (lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			if (lex.match("=")) {
+				lex.nextLex();
+				if (lex.tokenCategory() == Lexer.intToken ||
+				    lex.tokenCategory() == Lexer.realToken ||
+				    lex.tokenCategory() == Lexer.stringToken) {
+					lex.nextLex();
+				}
+				else {
+					parseError(31);
+				}
+			}
+			else {
+				parseError(20);
+			}
+		}
+		else {
+			parseError(27);
+		}
+		
+		stop("constantDeclaration");
+	}
+
+	private void typeDeclaration() throws ParseException {
+		start("typeDeclaration");
+		
+		if (lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			nameDeclaration();
+		}
+		else {
+			parseError(27);
+		}
+		
+		stop("typeDeclaration");
+	}
+
+	private void variableDeclaration() throws ParseException {
+		start("variableDeclaration");
+		
+		if (lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			nameDeclaration();
+		}
+		else {
+			parseError(27);
+		}
+		
+		stop("variableDeclaration");
+	}
+	
+	private void nameDeclaration() throws ParseException {
+		start("nameDeclaration");
+		
+		if (lex.match(":")) {
+			lex.nextLex();
+			type();
+		}
+		else {
+			parseError(19);
+		}
+		
+		stop("nameDeclaration");
+	}
+
+	private void classDeclaration () throws ParseException {
+		start("classDeclaration");
+		
+		if (lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			classBody();
+		}
+		else {
+			parseError(27);
+		}
+		
+		stop("classDeclaration");
+	}
+	
+	private void classBody () throws ParseException {
+		start("classBody");
+		
+		if (lex.match("begin")) {
+			lex.nextLex();
+			while (!lex.match("end")) {
+				nonClassDeclaration();
+			}
+			if (lex.match("end")) {
+				lex.nextLex();
+			}
+			else {
+				parseError(8);
+			}
+		}
+		else {
+			parseError(4);
+		}
+		
+		stop("classBody");
+	}
+	
+	private void functionDeclaration () throws ParseException {
+		start("functionDeclaration");
+		
+		if (lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			arguments();
+			returnType();
+			functionBody();
+		}
+		else {
+			parseError(27);
+		}
+		
+		stop("functionDeclaration");
+	}
+	
+	private void arguments () throws ParseException {
+		start("arguments");
+		
+		if (lex.match("(")) {
+			lex.nextLex();
+			argumentList();
+			if (lex.match(")") {
+				lex.nextLex();
+			}
+			else {
+				parseError(22);
+			}
+		}
+		else {
+			parseError(21);
+		}
+		
+		stop("arguments");
+	}
+	
+	private void argumentList () throws ParseException {
+		start("argumentList");
+		
+		if(lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			nameDeclaration();
+			while(!lex.match(")") {
+				if(lex.match(",")) {
+					lex.nextLex();
+					if(lex.tokenCategory() == Lexer.identifierToken) {
+						lex.nextLex();
+						nameDeclaration();
+					}
+					else {
+						parseError(27);
+					}					
+				}
+				else {
+					parseError(47, "expecting comma");
+				}
+			}
+		}
+		else {} //Successfully match nothing
+		
+		stop("argumentList");
+	}
+	
+	private void returnType () throws ParseException {
+		start("returnType");
+		
+		if(lex.match(":")) {
+			lex.nextLex();
+			type();
+		}
+		else {} //Successfully matched nothing
+		
+		stop("returnType");
+	}
+	
+	private void type () throws ParseException {
+		start("type");
+		
+		if( lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+		}
+		else if( lex.match("^")) {
+			lex.nextLex();
+			type();
+		}
+		else if( lex.match("[")) {
+			lex.nextLex();
+			if( lex.tokenCategory() == Lexer.intToken) {
+				lex.nextLex();
+				if( lex.match(":")) {
+					lex.nextLex();
+					if( lex.tokenCategory() == Lexer.intToken) {
+						lex.nextLex();
+						if( lex.match("]")) {
+							lex.nextLex();
+							type();
+						}
+						else {
+							parseError(24);
+						}
+					}
+					else {
+						parseError(32);
+					}
+				}
+				else {
+					parseError(19);
+				}
+			}
+			else {
+				parseError(32);
+			}
+		}
+		else {
+			parseError(30);
+		}
+		
+		stop("type");
+	}
+
+	private void functionBody () throws ParseException {
+		start("functionBody");
+		
+		while(!lex.match("begin")) {
+			nonClassDeclaration();
+			lex.nextLex();
+			if( lex.match(";")) {
+				lex.nextLex();
+			}
+			else {
+				parseError(18);
+			}
+		}
+		
+		compoundStatement();
+		
+		stop("functionBody");
+	}
+	
+	private void compoundStatement () throws ParseException {
+		start("compoundStatement");
+		
+		while(!lex.match("end")) {
+			statement();
+			lex.nextLex();
+			if( lex.match(";")) {
+				lex.nextLex();
+			}
+			else {
+				parseError(18);
+			}
+		}
+		lex.nextLex();
+		
+		stop("compoundStatement");
+	}
+	
+	private void statement () throws ParseException {
+		start("statement");
+		
+		if(lex.match("return")) {
+			lex.nextLex();
+			returnStatement();
+		}
+		else if(lex.match("if")) {
+			lex.nextLex();
+			ifStatement();
+		}
+		else if(lex.match("while")) {
+			lex.nextLex();
+			whileStatement();
+		}
+		else if(lex.match("begin")) {
+			lex.nextLex();
+			compoundStatement();
+		}
+		else if(lex.tokenCategory() == Lexer.identifierToken) {
+			lex.nextLex();
+			assignOrFunction();
+		}
+		else {
+			parseError(34);
+		}
+
+		stop("statement");
+
+	private void returnStatement () throws ParseException {
+		start("returnStatement");
+		
+		if(lex.match("(")) {
+			lex.nextLex();
+			expression();
+			lex.nextLex();
+			if(lex.match(")")) {
+				lex.nextLex();
+			}
+			else {
+				parseError(22);
+			}
+		}
+		else {} //Successfully matched nothing
+		
+		stop("returnStatement");
+	}
+
+	private void ifStatement () throws ParseException {
+		start("ifStatement");
+		
+		if(lex.match("(")) {
+			lex.nextLex();
+			expression();
+			lex.nextLex();
+			if(lex.match(")")) {
+				lex.nextLex();
+				statement();
+				if(lex.match("else")) {
+					lex.nextLex();
+					statement();
+				}
+				else {} //Successfully match nothing
+			}
+			else {
+				parseError(22);
+			}
+		}
+		else {
+			parseError(21);
+		}
+		
+		stop("ifStatement");
+	}
+	
+	private void whileStatement () throws ParseException {
+		start("whileStatement");
+
+		if(lex.match("(")) {
+			lex.nextLex();
+			expression();
+			lex.nextLex();
+			if(lex.match(")")) {
+				lex.nextLex();
+				statement();
+			}
+			else {
+				parseError(22);
+			}
+		}
+		else {
+			parseError(21);
+		}
+		
+		start("whileStatement");
+	}
+	
+	private void assignOrFunction () throws ParseException {
+		start("assignOrFunction");
+		
+		reference();
+		if(lex.match("=")) {
+			lex.nextLex();
+			expression();
+		}
+		else if(lex.match("(")) {
+			lex.nextLex();
+			parameterList();
+			if(lex.match(")")) {
+				lex.nextLex();
+			}
+			else {
+				parseError(22);
+			}
+		}
+		else {
+			parseError(47, "expecting '=' or '('");
+		}
+		
+		stop("assignOrFunction");
+	}
+	
+	private void parameterList () throws ParseException {
+		start("parameterList");
+		
+		while(!lex.match(")")) {
+			
+		}
+		
+		stop("parameterList");	
+	}
 }
