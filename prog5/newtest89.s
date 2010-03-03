0bf7afa4941a91ba3dd6a49a490a6073b1f6745e
Samuel Heinith <heiniths@kec1130-35.engr.oregonstate.edu>
2010-03-02 18:16:05 -0800

    added megatests

---
+++ prog5/newtest89.s
@@ -0,0 +1,119 @@
+Reading file /nfs/stak/a2/classes/eecs/winter2010/cs480/handin/Prog5Test/rootbr/test89.txt
+Begin function foo
+local space 0
+frame pointer
+Integer 8
+do addition Address to Integer
+dereference Integer
+frame pointer
+Integer 24
+do addition Address to Integer
+dereference Integer
+compare less Boolean
+Branch if False Label 1
+.L2:
+frame pointer
+Integer 20
+do addition Address to Integer
+dereference Integer
+frame pointer
+Integer 32
+do addition Address to Integer
+dereference Integer
+compare notEqual Boolean
+Branch if False Label 3
+String go go gadget
+push argument Pointer to Character
+Global printStr function type
+function call Primitive type
+branch to L2
+.L3:
+.L1:
+End function foo
+Begin function goGoGadget
+local space 48
+frame pointer
+Integer 8
+do addition Address to Integer
+dereference Integer
+push argument Integer
+Global printInt function type
+function call Primitive type
+frame pointer
+Integer -40
+do addition Address to Real
+dereference Real
+push argument Real
+Global printReal function type
+function call Primitive type
+frame pointer
+Integer -48
+do addition Address to Array 21 to 25 of Real 
+dereference Array 21 to 25 of Real 
+return from function 
+End function goGoGadget 
+Begin function main 
+local space 80 
+frame pointer 
+Integer -44 
+do addition Address to Real 
+frame pointer 
+Integer -32 
+do addition Address to Array 22 to 25 of Real 
+dereference Array 22 to 25 of Real 
+push argument Array 22 to 25 of Real 
+Real 4.5 
+push argument Real 
+Integer 3 
+push argument Integer 
+Global foo function type 
+function call Real 
+do assignment 
+frame pointer 
+Integer -64 
+do addition Address to Array 1 to 5 of Integer 
+frame pointer 
+Integer -36 
+do addition Address to Integer 
+dereference Integer 
+push argument Integer 
+Global goGoGadget function type 
+function call Array 1 to 5 of Integer 
+do assignment 
+frame pointer 
+Integer -36 
+do addition Address to Integer 
+Integer 11 
+frame pointer 
+frame pointer 
+Integer -36 
+do addition Address to Integer 
+dereference Integer 
+Integer 4 
+do multiplication Integer 
+do addition Address to Integer 
+Integer -60 
+do addition Address to Integer 
+dereference Integer 
+do multiplication Integer 
+Integer -7 
+do addition Integer 
+do assignment 
+frame pointer 
+Integer -36 
+do addition Address to Integer 
+frame pointer 
+frame pointer 
+Integer -36 
+do addition Address to Integer 
+dereference Integer 
+Integer 20 
+do multiplication Integer 
+Integer 4 
+do multiplication Integer 
+do addition Address to Integer 
+Integer -88 
+do addition Address to Integer 
+dereference Integer 
+do assignment 
+End function main
